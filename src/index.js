fc96428a3c11dabcc81a6ed906c56dda8baa02ee
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-09 11:51:45 -0800

    add converter select options
    Co-authored-by: Hayley M <hayleymcvay8@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import $ from "jquery";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import Crypto from "./js/crypto.js";
 
 function getElements(response) {
-  if (response.main) {
-    const body = JSON.parse(response);
+  if (response) {
+    // const rankArray = [];
+    // const currArray = [];
+    const multArray = [];
+    $("ul#rank").append(`<li>Rank</li>`);
+    $("ul#currency").append(`<li>Currency</li>`);
+    $("ul#market_cap").append(`<li>Market Cap</li>`);
     for (let i = 0; i < 50; i++) {
-      const currency = body[i].currency;
-      const rank = body[i].rank;
-      const market_cap = body[i].market_cap;
+      const currency = response[i].currency;
+      const rank = response[i].rank;
+      const market_cap = response[i].market_cap;
       $("ul#rank").append(`<li>${rank}</li>`);
       $("ul#currency").append(`<li>${currency}</li>`);
       $("ul#market_cap").append(`<li>$ ${market_cap}.00</li>`);
+      multArray.push([currency, rank]);
+    }
+    multArray.sort();
+    console.log(multArray);
+    for (let i = 0; i < multArray.length; i++) {
+      $("select.currencies-list").append(`<option value=${multArray[i][1] - 1}>${multArray[i][0]}</option>`);
     }
   } else {
-    $(".showErrors").text(`There was an error processing your request: ${error}`);
+    $(".showErrors").text(`There was an error: ${response}`);
   }
 }
 
@@ -28,6 +37,9 @@ async function makeApiCall(id, interval) {
 $(document).ready(function () {
   let id = "";
   let interval = "";
-
+  const dateObj = new Date();
   makeApiCall(id, interval);
+  $("#date").html(dateObj);
+
+  $("#convert").click(function () {});
 });

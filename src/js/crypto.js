1db2eb0af91d0c6289169963f6a026bed631235d
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-09 09:50:51 -0800

    add API call
    Co-authored-by: Hayley M <hayleymcvay8@gmail.com>

--- src/js/crypto.js
+++ src/js/crypto.js
@@ -1,3 +1,13 @@
 export default class Crypto {
-  constructor() {}
+  static async getCrypto(location) {
+    try {
+      const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${id}&interval=${interval}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
 }

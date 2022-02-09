1f41c8a13ff189a90a61dee7243b74b22101c391
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-09 10:09:02 -0800

    add UI logic for API call
    Co-authored-by: Hayley M <hayleymcvay8@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,4 +4,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import Crypto from "./js/crypto.js";
 
-$(document).ready(function () {});
+function getElements(response) {
+  if (response.main) {
+    const body = JSON.parse(response);
+    for (let i = 0; i < 50; i++) {
+      const currency = body[i].currency;
+      const rank = body[i].rank;
+      const market_cap = body[i].market_cap;
+      $("ul#rank").append(`<li>${rank}</li>`);
+      $("ul#currency").append(`<li>${currency}</li>`);
+      $("ul#market_cap").append(`<li>$ ${market_cap}.00</li>`);
+    }
+  } else {
+    $(".showErrors").text(`There was an error processing your request: ${error}`);
+  }
+}
+
+async function makeApiCall(id, interval) {
+  const response = await Crypto.getCrypto(id, interval);
+  getElements(response);
+}
+
+$(document).ready(function () {
+  let id = "";
+  let interval = "";
+
+  makeApiCall(id, interval);
+});
